c9f5824c35fe47c91b0bf1b3d3c4718c54e71a15
Richard Palmer <rich@rdjpalmer.com>
2021-10-29 12:34:55 +0100

    Fix test

--- src/figma-code.spec.ts
+++ src/figma-code.spec.ts
@@ -1,9 +1,8 @@
 import { setupIdentification } from "./figma-code";
 
 const mockFigma = {
-  clientStorage: {
-    getAsync: jest.fn(() => Promise.resolve("")),
-    setAsync: jest.fn(() => Promise.resolve("")),
+  currentUser: {
+    id: "1234",
   },
   ui: {
     postMessage: jest.fn(),
@@ -15,33 +14,8 @@ describe("setupIdentification", () => {
     jest.resetAllMocks();
   });
 
-  it("generates a uuid if one does not exist", async () => {
-    // @ts-expect-error
+  it("returns the figma user's ID", async () => {
     await setupIdentification(mockFigma);
-
-    expect(mockFigma.clientStorage.setAsync).toHaveBeenCalledTimes(1);
-    expect(mockFigma.clientStorage.setAsync).toHaveBeenCalledWith(
-      "mixpanel-distinct-id",
-      expect.any(String)
-    );
-
-    expect(mockFigma.ui.postMessage).toBeCalledTimes(1);
-    expect(mockFigma.ui.postMessage).toBeCalledWith(
-      expect.objectContaining({
-        type: "mixpanel-identification",
-        userId: expect.any(String),
-      })
-    );
-  });
-
-  it("returns the existing ID if one exists", async () => {
-    mockFigma.clientStorage.getAsync.mockImplementation(() =>
-      Promise.resolve("1234")
-    );
-    // @ts-expect-error
-    await setupIdentification(mockFigma);
-
-    expect(mockFigma.clientStorage.setAsync).toHaveBeenCalledTimes(0);
 
     expect(mockFigma.ui.postMessage).toBeCalledTimes(1);
     expect(mockFigma.ui.postMessage).toBeCalledWith(
