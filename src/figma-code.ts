dd3e70e20646c7cab0379202e6af00ff21075006
Richard Palmer <rich@rdjpalmer.com>
2021-10-29 12:17:16 +0100

    Use figma.currentUser.id instead of generated id

--- src/figma-code.ts
+++ src/figma-code.ts
@@ -1,41 +1,19 @@
-function createUUID() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
-
-interface ClientStorageAPI {
-  getAsync(key: string): Promise<any>;
-  setAsync(key: string, value: any): Promise<void>;
+interface CurrentUser {
+  id: string;
 }
 
 interface UIAPI {
-  show(): void;
-  hide(): void;
-  resize(width: number, height: number): void;
-  close(): void;
-
   postMessage(pluginMessage: any, options?: UIPostMessageOptions): void;
-  onmessage: MessageEventHandler | undefined;
-  on(type: "message", callback: MessageEventHandler): void;
-  once(type: "message", callback: MessageEventHandler): void;
-  off(type: "message", callback: MessageEventHandler): void;
 }
 
 interface Figma {
-  readonly clientStorage: ClientStorageAPI;
+  readonly currentUser: CurrentUser;
   readonly ui: UIAPI;
 }
 
 export async function setupIdentification(figma: Figma) {
-  let userId = await figma.clientStorage.getAsync("mixpanel-distinct-id");
-
-  if (!userId) {
-    userId = createUUID();
-    figma.clientStorage.setAsync("mixpanel-distinct-id", userId);
-  }
-
-  figma.ui.postMessage({ type: "mixpanel-identification", userId });
+  figma.ui.postMessage({
+    type: "mixpanel-identification",
+    userId: figma.currentUser.id,
+  });
 }
