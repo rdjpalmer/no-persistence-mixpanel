a027767fecb966d43ed04b3473ae841667d3800a
Richard Palmer <rich@rdjpalmer.com>
2022-03-22 17:06:18 +0000

    Update figma helper

--- src/figma-ui.spec.ts
+++ src/figma-ui.spec.ts
@@ -1,20 +1,49 @@
 import Mixpanel from ".";
-import { init } from "./figma-ui";
+import { withMixpanel } from "./figma-ui";
 
-function makeMockFigmaMessage() {
-  return {
-    data: {
-      pluginMessage: { type: "mixpanel-identification", userId: "1234" },
-    },
-  };
+function makeMockFigmaMessage(pluginMessage: Record<string, string>) {
+  return { data: { pluginMessage } };
 }
 
 describe("Figma UI initialiser", () => {
-  it("returns a mixpanel instance when the identification event is fired", async () => {
-    init("dummy-token", (mixpanel) => {
-      expect(mixpanel).toBeInstanceOf(Mixpanel);
-    });
+  it("ignores non-mixpanel related messages", () => {
+    window.postMessage(makeMockFigmaMessage({ name: "test-message" }), "*");
 
-    window.postMessage(makeMockFigmaMessage(), "*");
+    window.onmessage = withMixpanel(
+      (event) => {
+        expect(event).toMatchObject({
+          data: { pluginMesssage: { name: "test-message" } },
+        });
+      },
+      { token: "1234" }
+    );
+  });
+
+  it("provides the user-land message handler with the mixpanel instance", () => {
+    window.postMessage(makeMockFigmaMessage({ name: "test-message" }), "*");
+
+    window.onmessage = withMixpanel(
+      (event, mixpanel) => {
+        expect(mixpanel).toBeInstanceOf(Mixpanel);
+      },
+      { token: "1234" }
+    );
+  });
+
+  it("identifies the user", async () => {
+    window.postMessage(
+      makeMockFigmaMessage({ type: "mixpanel-identification", userId: "1234" }),
+      "*"
+    );
+    window.postMessage(makeMockFigmaMessage({ name: "test-message" }), "*");
+
+    window.onmessage = withMixpanel(
+      async (event, mixpanel) => {
+        expect(
+          mixpanel.track("Screen Viewed", { loggedIn: true })
+        ).resolves.not.toThrow();
+      },
+      { token: "1234" }
+    );
   });
 });
--- src/figma-ui.ts
+++ src/figma-ui.ts
@@ -15,3 +15,26 @@ export function init(
     }
   };
 }
+
+type OnMessageHandler = (ev: MessageEvent<any>, mixpanel: Mixpanel) => any;
+interface MixpanelOptions {
+  token: string;
+}
+
+export function withMixpanel(
+  onMessage: OnMessageHandler,
+  options: MixpanelOptions
+) {
+  const mixpanel = new Mixpanel(options.token);
+
+  return (event: MessageEvent<any>) => {
+    onMessage(event, mixpanel);
+
+    // if (typeof event?.data?.type !== "undefined" && typeof event?.data?.userId !== "undefined" && event.data.type === "mixpanel-identify") {
+    //   const message = event.data.pluginMessage;
+    //   mixpanel.identify(message.userId);
+    // } else {
+    //   onMessage()
+    // }
+  };
+}
